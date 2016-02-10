a2bafc18e26c9671b487011700149210cc373de9
jltran <guardian@berkeley.edu>
2016-02-10 14:11:36 -0800

    include onload animation for first block. group svgs for block2

--- js/src/main.js
+++ js/src/main.js
@@ -2,11 +2,26 @@
 //Size blocks
 var blocks = [].slice.call(document.getElementsByClassName('block'));
 
+//set block height equal to width. re-run on window resize.
 function resizeBlocks(){
   blocks.forEach(function(d){
     d.setAttribute('style', 'height:' + d.offsetWidth + 'px');
   });
 }
-
 window.onresize = resizeBlocks;
 resizeBlocks();
+
+//Animate using gsap
+function animatePhone(){
+  var phoneSvg = [].slice.call(document.getElementsByClassName('phone-svg'));
+  console.log(phoneSvg);
+  phoneSvg.forEach(function(thisPhoneSvg){
+    var svgDoc = thisPhoneSvg.contentDocument;
+    var phoneObject = svgDoc.getElementById('phone');
+    TweenLite.from(phoneObject, 1, {x: '-=200px', autoAlpha: 0});
+  });
+}
+
+window.onload = animatePhone;
+
+//beforeunload <- should remove event handlers
